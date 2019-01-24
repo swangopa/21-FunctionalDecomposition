993000b010298f786d35897727461dd2ba3f804a
swangopa <swangopa@rose-hulman.edu>
2019-01-24 13:21:27 -0500

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,43 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Paige Swango and Miguel Cooper.
+"""  # DONE: 1. .
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 ####### Do NOT attempt this assignment before class! #######
+import random
+def main():
+    length = length_word()
+    while True:
+        word = random_word()
+        if length <= len(word):
+            break
+    print(word)
+    over = input("How manyy unsuccessful choices do you want to allow yourself?")
+    guess = input("What letter do you want to try?")
+    print(guess)
 
+
+
+
+def random_word():
+    with open('words.txt') as f:
+        f.readline()
+        string = f.read()
+        words = string.split()
+
+        r = random.randrange(0,len(words))
+        word = words[r]
+        print(word)
+        return word
+
+def length_word():
+    number = input("what minimum length do you want for the secret word?")
+    number = int(number)
+    return number
+
+
+
+main()
